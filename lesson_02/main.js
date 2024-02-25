b2b87b93c41f1b6a984e15f1f16668101c04d857
andrii vorflik <andrii@gmail.com>
2024-02-25 16:03:57 +0200

    added code from lesson 2

---
+++ lesson_02/main.js
@@ -0,0 +1,163 @@
+// Number
+// let integer = 1000;
+// let float = 12.23;
+
+
+// console.log(typeof integer);
+// console.log(typeof float);
+// console.log(Infinity, -Infinity, NaN);
+// console.log("Hello" / 3); // NaN  not a number
+
+
+// String
+// let someString = "world";
+// let char = 'h';
+// let string1 = 'Hello world!';
+// let string2 = "Hello world!";
+// let string3 = `Hello ${someString}!`;
+
+// console.log(typeof string);
+// console.log(typeof char);
+
+
+// Boolean
+// let booleanTrueValue = true;
+// let booleanFalseValue = false;
+
+// console.log(typeof booleanTrueValue);
+// console.log(typeof booleanFalseValue);
+
+
+
+
+// Undefined
+
+// let variable;
+// console.log(typeof variable);
+
+// variable = 10;
+// console.log(variable);
+
+// variable = undefined;
+// console.log(variable);
+
+
+// Null
+
+// let variable = null;
+// console.log(typeof variable);
+
+
+
+// Object
+
+// let user = {
+//   firstName: 'James',
+//   lastName: 'Bond',
+// }
+
+// console.log('user:', user);
+// console.log(typeof user);
+
+
+
+
+// Symbol
+// let symbol = Symbol('123');
+// console.log('symbol:', symbol);
+
+
+
+
+// BigInt
+
+// let num1 = 12n;
+// let num2 = 2n;
+
+// console.log(typeof num1);
+// console.log(typeof num2);
+
+// console.log(num1 + num2); // 14n
+
+// console.log(num1 + 3);  // Error!
+
+
+
+
+// console.log(typeof 0); // number
+// console.log(typeof 'some string'); // string
+// console.log(typeof true); // boolean
+// console.log(typeof undefined); // undefined
+// console.log(typeof Symbol("id")); // symbol
+// console.log(typeof 10n); // bigint
+// console.log(typeof {}); // object
+// console.log(typeof null); // object ERROR in JS!
+// console.log(typeof alert); // function
+
+
+
+
+// String
+
+// console.log(123, '123');
+// console.log(true, 'true');
+// console.log(undefined, 'undefined');
+// console.log(null, 'null');
+
+// console.log( String(123))
+
+// Number
+// console.log('123');
+// console.log('  123  ');
+// console.log('123 hi');
+// console.log(Number(true));
+// console.log(Number(false));
+// console.log(Number(null));
+// console.log(Number(undefined));
+// console.log(Number(1231231231223132123132131213113123123123123n));
+
+
+
+// Boolean
+// '' 0 NaN null undefined 0n - false
+// console.log(Boolean(0));
+
+
+
+
+// Оператори
+
+// console.log(6 % 2); // 0
+// console.log(10 % 3); // 1
+// console.log(8 % 3); // 2
+
+// console.log(2 ** 3); // 2*2*2
+
+// console.log(2 + 3);
+// console.log(3 + '    2 ');
+
+// console.log(4 ** 0.5);
+
+// let num1 = undefined;
+// console.log(+num1);
+
+// let salary = 800;
+
+// salary = salary + 400;
+// salary += 400;
+// salary %= 2;
+
+// console.log('salary:', salary);
+
+// Increment decrement
+
+// let num = 5;
+
+// num = num + 1;
+// num += 1;
+// num++;
+// num--;
+
+
+
+
